98c47cc7fe8f0c1dd319a058f982b40786c7a75c
Aniket Patel <patelaniket@kccemsr.edu.in>
2021-11-10 13:11:47 +0530

    Final exam

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * rate * years / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    if (principal <= 0) {
+        alert("Enter a positive number!");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+    var result = "If you deposit <mark>" + principal + "</mark>,<br />at an interest rate of <mark>" + rate + "%</mark>. <br />You will receive an amount of <mark>" + interest + "</mark>,<br />in the year <mark>" + year + "</mark>";
+    document.getElementById("result").innerHTML = result;
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
